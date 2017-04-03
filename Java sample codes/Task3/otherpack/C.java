f12b448942f122428e60eba22f93728d2637195f
DESKTOP-LRLP302\vardhan <harshapalvayiactive@gmail.com>
2017-04-02 19:13:21 -0500

    task3 code

---
+++ Java sample codes/Task3/otherpack/C.java
@@ -0,0 +1,9 @@
+package otherpack;
+import pack.*;
+public class C {
+	public static void main(String args[])
+	  {
+	    A a = new A();
+	    a.info();
+	  }
+}
---
+++ Java sample codes/Task3/pack/A.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class A {
+	 public void info()
+	  {
+	    System.out.println("hello");
+	  }
+	  protected void msg()
+	  {
+	    System.out.println("hi");
+	  }
+
+}
