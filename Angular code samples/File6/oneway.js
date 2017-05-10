c3aa1374fa1c0ff23de2895c6cd6136f61c060f0
DESKTOP-LRLP302\vardhan <harshapalvayiactive@gmail.com>
2017-05-10 14:14:59 -0500

    commit

---
+++ Angular code samples/File6/oneway.js
@@ -0,0 +1,6 @@
+var app = angular.module("app",[]);
+app.controller("emp",["$scope", function($scope){
+	$scope.a = 10;
+	$scope.b = 30;
+}])
+	  
---
+++ Angular code samples/File6/twoway.js
@@ -0,0 +1,10 @@
+var app = angular.module("app",[]);
+app.controller("emp",["$scope", function($scope){
+	$scope.a = 10;
+	$scope.b = 30;
+	$scope.doSum = function()
+	{
+		$scope.sum = parseInt($scope.a) + parseInt($scope.b);
+	};
+}])
+	  
